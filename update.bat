dca8f14e8e1b23fb4655655cd0d4f63b44b9c1a3
amongothers <zhengwenwei.123@gmail.com>
2013-01-21 11:48:39 +0800

    daily

--- update.bat
+++ update.bat
@@ -1,7 +1,5 @@
 git add *
 git add -u *
 set commit_msg="daily"
-echo %commit_msg%
-if not exist %1 ( set commit_msg = %1)
-echo %commit_msg%
+if exist %1 ( set commit_msg=%1)
 git commit -m %commit_msg%
